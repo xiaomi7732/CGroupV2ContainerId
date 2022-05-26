26ded011d9b7e6a3539434b73433ffe284216ba9
Saar Shen <xiaomi7732@hotmail.com>
2022-05-26 16:44:00 -0700

    Add CLI

---
+++ src/CodeWithSaar.ContainerId.CLI/Program.cs
@@ -0,0 +1,8 @@
+﻿using CodeWithSaar.Extensions.ContainerId;
+
+string containerId = await ContainerService.GetContainerIdAsync();
+
+if (string.IsNullOrEmpty(containerId))
+{
+    Console.WriteLine("No container id found. Is this inside a container?");
+}
